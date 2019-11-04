59a182c14acc29b9ff6e9dd83132e6971573ded9
Allison Patton <allison.tpttn@gmail.com>
2019-11-04 11:37:26 -0600

    working on cohort controller but have error on GetAllCohorts

--- StudentExercisesAPI/Controllers/CohortController.cs
+++ StudentExercisesAPI/Controllers/CohortController.cs
@@ -39,59 +39,98 @@ namespace StudentExercisesAPI.Controllers
                 conn.Open();
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = $"SELECT Id, Name FROM Cohort";
+                    cmd.CommandText = @"
+                                        SELECT c.Id, c.CohortName, s.Id AS 'StudentId', s.FirstName, s.LastName, s.SlackHandle
+                                        i.Id AS 'InstructorId', i.FirstName, i.LastName, i.SlackHandle, i.Speciality
+                                        FROM Cohort c LEFT JOIN Student s ON s.CohortId = c.Id    
+                                        LEFT JOIN Instructor i ON i.InstuctorId = c.Id";
                     SqlDataReader reader = cmd.ExecuteReader();
 
-                    List<Student> students = new List<Student>();
+                    Dictionary<int, Cohort> cohorts = new Dictionary<int, Cohort>();
 
                     while (reader.Read())
                     {
-                        Student student = new Student
+                        int cohortId = reader.GetInt32(reader.GetOrdinal("Id")); //get the id
+                        if (!cohorts.ContainsKey(cohortId)) //have I seen this student before?
                         {
-                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
-                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
-                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
-                            SlackHandle = reader.GetString(reader.GetOrdinal("SlackHandle")),
-                            CohortId = reader.GetInt32(reader.GetOrdinal("CohortId"))
-                        };
-                        students.Add(student);
-                    }
-                    reader.Close();
+                            Cohort newCohort = new Cohort
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                StudentList = new List<Student>(),
+                                InstructorList = new List<Instructor>()
 
-                    /*
-                        The Ok() method is an abstraction that constructs
-                        a new HTTP response with a 200 status code, and converts
-                        your IEnumerable into a JSON string to be sent back to
-                        the requessting client application.
-                    */
-                    return Ok(students);
+                            };
+                            cohorts.Add(cohortId, newCohort);
+                        }
+
+                        Cohort fromDictionary = cohorts[cohortId];
+                        if (!reader.IsDBNull(reader.GetOrdinal("StudentId")))
+                        {
+                            Student newStudent = new Student()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("StudentId")),
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")), //need to disambiguate
+                                LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                                SlackHandle = reader.GetString(reader.GetOrdinal("SlackHandle"))
+                            };
+                            fromDictionary.StudentList.Add(newStudent);
+                        }
+                        if (!reader.IsDBNull(reader.GetOrdinal("InstructorId")))
+                        {
+                            Instructor newInstructor = new Instructor()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("StudentId")),
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")), //need to disambiguate
+                                LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                                SlackHandle = reader.GetString(reader.GetOrdinal("SlackHandle")),
+                                Speciality = reader.GetString(reader.GetOrdinal("Speciality"))
+                            };
+                            fromDictionary.InstructorList.Add(newInstructor);
+                        }
+
+
+                        reader.Close();
+
+                        /*
+                            The Ok() method is an abstraction that constructs
+                            a new HTTP response with a 200 status code, and converts
+                            your IEnumerable into a JSON string to be sent back to
+                            the requessting client application.
+                        */
+                        return Ok(cohorts.Values);
+                    }
                 }
             }
         }
-
-        // GET: api/Cohort/5
-        [HttpGet("{id}", Name = "GetCohort")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/Cohort
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT: api/Cohort/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
+
+
+
+    // GET: api/Cohort/5
+    //[HttpGet("{id}", Name = "GetCohort")]
+    //public string Get(int id)
+    //{
+    //    return "value";
+    //}
+
+    //// POST: api/Cohort
+    //[HttpPost]
+    //public void Post([FromBody] string value)
+    //{
+    //}
+
+    //// PUT: api/Cohort/5
+    //[HttpPut("{id}")]
+    //public void Put(int id, [FromBody] string value)
+    //{
+    //}
+
+    //// DELETE: api/ApiWithActions/5
+    //[HttpDelete("{id}")]
+    //public void Delete(int id)
+    //{
+    //}
 }
+
+
